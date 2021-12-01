11575f1d515d02ac3945caab225cd3daddb883fd
Wayne Greeley <it199@hotmail.com>
2021-11-30 20:55:31 -0600

    add base files

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ My new favorite npm package manager...
 npm install -g npm-check-updates
 ncu -u
 ```
+
+```
+npm init -y
+npm i -D jest
+
+npm run compile
+node .\built\read-file-test.js
+```
---
+++ src-ts/read-file-test.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import * as rd from 'readline'
+
+var reader = rd.createInterface(fs.createReadStream("C:\\appdev\\vs\\adventofcode2021\\text-assets\\pokemon.txt"))
+
+//id,identifier,species_id,height,weight,base_experience,order,is_default
+var data: Array<{ 
+    pokemonId: number; 
+    pokemonIdentifier: string; 
+    pokemonSpecies: string; 
+    pokemonHeight: string; 
+    pokemonWeight: string; 
+    pokemonBaseExp: string; 
+    pokemonOrder: string; 
+    pokemonIsDefault: string
+}> = [];
+
+reader.on("line", (l: string) => {
+    const tokens = l.split(',');
+    const id = parseInt(tokens[0]);
+    const pokemonIdentifier = tokens[1];
+    const pokemonSpecies = tokens[2];
+    const pokemonHeight = tokens[3];
+    const pokemonWeight = tokens[4];
+    const pokemonBaseExp = tokens[5];
+    const pokemonOrder = tokens[6];
+    const pokemonIsDefault = tokens[7];
+    // console.log(`id: ${id} from ${pokemonIdentifier} to ${pokemonSpecies}`);
+    data.push({
+        pokemonId: id, 
+        pokemonIdentifier, 
+        pokemonSpecies, 
+        pokemonHeight, 
+        pokemonWeight, 
+        pokemonBaseExp, 
+        pokemonOrder, 
+        pokemonIsDefault
+    });
+})
+console.log(`Will be empty data has not yet been read ${data.length}` );
+
+reader.on("close", ()=> {
+    console.log(`Data has been read ${data.length}` );
+    data.forEach(element => {
+        console.log(`id: ${element.pokemonId} from ${element.pokemonIdentifier} to ${element.pokemonSpecies}`)
+    });
+})
